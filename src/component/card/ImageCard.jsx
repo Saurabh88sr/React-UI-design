d0a07b815f9a861fe28c08779654d7ae9413b199
saur987 <saurabhverma@ashrishait.com>
2024-12-05 01:07:22 +0530

    multicard section added

---
+++ src/component/card/ImageCard.jsx
@@ -0,0 +1,22 @@
+import imag1 from "../../img/imag1.jpeg";
+import ArrowOutwardOutlinedIcon from '@mui/icons-material/ArrowOutwardOutlined';
+const ImageCard = () => {
+  return (
+    <div className="max-w-sm rounded-3xl p-3 overflow-hidden shadow-lg bg-white">
+      <div className="px-2 py-4">
+        <div className="flex justify-between">
+          <h2 className="font-bold text-xl mb-2">Main Heading</h2>
+          <div className="text-white p-2 rounded-full bg-gray-700"><ArrowOutwardOutlinedIcon/></div>
+        </div>
+        <p className="text-gray-700 text-base">
+          This is a brief description of the card. You can use this space to
+          summarize the content or purpose of the card.
+        </p>
+      </div>
+
+      <img className="w-full p-2 rounded-3xl" src={imag1} alt="Sample Image" />
+    </div>
+  );
+};
+
+export default ImageCard;
--- src/page/mainpage/MultipleCard.jsx
+++ src/page/mainpage/MultipleCard.jsx
@@ -1,10 +1,18 @@
+import ImageCard from "../../component/card/imageCard";
 
 const MultipleCard = () => {
   return (
     <div>
-      <h1>this is miltipler card</h1>
+      <div className=" text-center pb-10">
+          <h2 className="text-4xl font-semibold">Multi Card Section</h2>
+        </div>
+      <div className="flex justify-around">
+        <ImageCard />
+        <ImageCard />
+        <ImageCard />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default MultipleCard
+export default MultipleCard;
